d8751d271f25cde93db4bebb821a81a6a4f86d68
Shawn <smithautoguy123@gmail.com>
2017-11-01 20:58:16 -0700

    dayonetakehome

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 var name = "Shawn A. Clarkson";
 
-var message = "I am hoping to learn javascript so that I can dive into some back-end based development and help with future projects my new business is pursuing, " + name + "!";
+var message = "I am hoping to learn javascript so that I can dive into some back-end based development and help with future projects my new business is pursuing, im really excited, " + name + "!";
 console.log(message);
